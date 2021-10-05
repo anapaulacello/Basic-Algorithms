82d5c02422c3ad8ac6d21c4775b8bd30fed86a08
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-10-05 14:29:45 +0200

    iteration5 y 6

---
+++ iteration6.js
@@ -0,0 +1,18 @@
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+    
+}
+console.log("---------");
+for (let index = 0; index < 10; index++) {
+    if (index%2==0) {
+        console.log(index);
+    }
+    
+}
+console.log("---------");
+for (let index = 0; index < 11; index++) {
+    console.log("intentando dormir");
+    if(index==10){
+        console.log("dormido");
+    }
+}
